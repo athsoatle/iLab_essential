2f89ffb1bf320c0ba62683e14b1c902c89786b7d
635h3n1y d073r <43099611+athsoatle@users.noreply.github.com>
2018-11-17 10:34:17 +0300

    Update onegin0_1.cpp

--- onegin/onegin0_1.cpp
+++ onegin/onegin0_1.cpp
@@ -24,16 +24,11 @@ using namespace std;
 ifstream fin("input.in");/*file opening*/
 ofstream fout("output.out");
 
-vector <string> textArray;
-vector <string> sortedArray;
-vector <string> convertedText;
-vector <pair <string, string> > ans;
 
-
-void readFile() {/*reading file*/
+void readFile(vector<string> * textArray) {/*reading file*/
 	string curStr;
 	while (getline(fin, curStr)) {
-		textArray.push_back(curStr);
+		textArray->push_back(curStr);
 	}
 }
 
@@ -41,43 +36,39 @@ string stringConverter(string str) {
 	string strRet = "";
 	for (unsigned int i = 0; i < str.length(); i++) {
 		if (isupper(str[i]))	strRet.push_back(tolower(str[i]));
-		else if (ispunct(str[i]))	pass;
+		else if (ispunct(str[i]) || str[i] == ' ')	pass;
 		else	strRet.push_back(str[i]);
 	}
 	return strRet;
 }
 
-void sortByBegin(){
-	sort(ans.begin(), ans.end());
-}
-
-void sortByEnds(){
-	for(unsigned i = 0; i < textArray.size(); i++){
-        reverse(ans[i].first.begin(), ans[i].first.end());
-    }
-    sort(ans.begin(), ans.end());
-}
-
-void printFile() {
-	for (unsigned int i = 0; i < textArray.size(); i++) {
-		fout << ans[i].second << endl;
+void printFile(unsigned int arraySize, vector<pair <string, string> > * ans) {
+	for (unsigned int i = 0; i < arraySize; i++) {
+		fout << (*ans)[i].second << "**---**" << (*ans)[i].first << endl;
 	}
 	fout << endl;
 }
 
 int main(int argc, char**argv) {
 	setlocale(LC_ALL, "Russian");
-	readFile();
-	for(unsigned i = 0; i < textArray.size(); i++){
-        convertedText.push_back(stringConverter(textArray[i]));
+	vector <string> textArray;
+	vector <string> convertedText;
+	vector <pair <string, string> > ans;
+	readFile(&textArray);
+	unsigned arraySize = textArray.size();
+	for (unsigned i = 0; i < arraySize; i++) {
+		convertedText.push_back(stringConverter(textArray[i]));
 	}
-	for(unsigned i = 0; i < textArray.size(); i++){
-        ans.push_back(make_pair(convertedText[i], textArray[i]));
+	for (unsigned i = 0; i < arraySize; i++) {
+		ans.push_back(make_pair(convertedText[i], textArray[i]));
 	}
-	sortByBegin();
-	printFile();
-	sortByEnds();
-	printFile();
+	sort(ans.begin(), ans.end());
+	printFile(arraySize, &ans);
+	for (unsigned i = 0; i < arraySize; i++) {
+		reverse(ans[i].first.begin(), ans[i].first.end());
+	}
+	sort(ans.begin(), ans.end());
+	printFile(arraySize, &ans);
 	system("PAUSE");
 	return 0;
 }
