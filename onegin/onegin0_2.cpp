8d4bd3a48ce5f6245c70330ec4b4bfd21916820a
635h3n1y d073r <43099611+athsoatle@users.noreply.github.com>
2018-11-18 19:27:40 +0300

    Update onegin0_2.cpp

--- onegin/onegin0_2.cpp
+++ onegin/onegin0_2.cpp
@@ -25,28 +25,11 @@ ifstream fin("input.in");/*file opening*/
 ofstream fout("output.out");
 
 
-void readFile(vector<string> * textArray) {/*reading file*/
-	string curStr;
-	while (getline(fin, curStr)) {
-		textArray->push_back(curStr);
-	}
-}
+void readFile(vector<string> * textArray);
 
-string stringConverter(string str) {
-	string strRet = "";
-	for (unsigned int i = 0; i < str.length(); i++) {
-		if (isupper(str[i]))	strRet.push_back(tolower(str[i]));
-		else if (islower(str[i]))	strRet.push_back(str[i]);
-	}
-	return strRet;
-}
+string stringConverter(string str);
 
-void printFile(unsigned int arraySize, vector<pair <string, string> > * ans) {
-	for (unsigned int i = 0; i < arraySize; i++) {
-		if((*ans)[i].second != "")	fout << (*ans)[i].second << endl;
-	}
-	fout << endl;
-}
+void printFile(unsigned int arraySize, vector<pair <string, string> > * ans);
 
 int main(int argc, char**argv) {
 	setlocale(LC_ALL, "Russian");
@@ -70,4 +53,27 @@ int main(int argc, char**argv) {
 	printFile(arraySize, &ans);
 	system("PAUSE");
 	return 0;
+}
+
+void readFile(vector<string> * textArray) {/*reading file*/
+	string curStr;
+	while (getline(fin, curStr)) {
+		textArray->push_back(curStr);
+	}
+}
+
+string stringConverter(string str) {
+	string strRet = "";
+	for (unsigned int i = 0; i < str.length(); i++) {
+		if (isupper(str[i]))	strRet.push_back(tolower(str[i]));
+		else if (islower(str[i]))	strRet.push_back(str[i]);
+	}
+	return strRet;
+}
+
+void printFile(unsigned int arraySize, vector<pair <string, string> > * ans) {
+	for (unsigned int i = 0; i < arraySize; i++) {
+		if((*ans)[i].second != "")	fout << (*ans)[i].second << endl;
+	}
+	fout << endl;
 }
