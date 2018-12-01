6ea8c38f029ff460be76e558813846d2b796539b
635h3n1y d073r <43099611+athsoatle@users.noreply.github.com>
2018-12-01 08:35:28 +0300

    Create includes.h

---
+++ list/includes.h
@@ -0,0 +1,23 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#define pass (void)0
+
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <fstream>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <list>
